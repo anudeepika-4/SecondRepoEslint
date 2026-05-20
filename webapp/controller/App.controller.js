f9160859b54fbe5733ee7e890de68e8818fe2546
Srikanth Gopu <srikanth.g@releaseowl.com>
2026-05-20 17:46:42 +0000

    initial setup

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("ns.html5module.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["ns/html5module/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
