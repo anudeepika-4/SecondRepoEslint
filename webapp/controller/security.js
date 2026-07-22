6ca66aa5ec1762353d99cc7a79d15dbcc72b879b
anudeepika-4 <atchutaanudeepika4@gmail.com>
2026-07-22 22:07:15 +0530

    Create security.js for owsap issues

---
+++ webapp/controller/security.js
@@ -0,0 +1,13 @@
+const password = "Admin@123";
+const apiKey = "AKIAIOSFODNN7EXAMPLE";
+
+console.log(password);
+console.log(apiKey);
+
+// Hardcoded secret
+const password = "Admin@123";
+const apiKey = "AKIAIOSFODNN7EXAMPLE";
+
+// Dangerous eval
+const input = "alert('Hello')";
+eval(input);
